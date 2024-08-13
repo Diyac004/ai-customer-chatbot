0599b6c30d1679781d1abd3a2e0f7ccff6633526
Diyac004 <diyachaudhari004@gmail.com>
2024-08-13 15:34:09 -0700

    first commit

---
+++ app/api/chat/route.js
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+export async function POST(req) {
+    const systemPrompt = "Welcome to your personalized shopping friend."
+    const openai = new OpenAI;
+    const data = await req.json()
+    const completion = await openai.chat.completions.create({
+        messages:[{
+            role: 'system',
+            content : systemPrompt,
+        },
+        ...data,
+    ],
+    model: "gpt-4o-mini",
+    stream: true
+    })
+    const stream = new ReadableStream({
+        async start(controller){
+            const encoder =new TextEncoder()
+            try{
+                for await(const chunk of completion){
+                    const content = chunk.choices[0]?.delta?.content
+                    if(content){
+                        const text=encoder.encode(content)
+                        controller.enqueue(text)
+                    }
+                }
+
+            }
+            catch(err){
+                controller.error(err)
+            } finally{
+                controller.close()
+            }
+        }
+    })
+    return new NextResponse(stream)
+}
